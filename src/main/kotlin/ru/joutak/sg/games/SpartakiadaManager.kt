08bfab16418b40201eb2960ae6aad7a757d48fbf
sadnex <s4dnex@gmail.com>
2025-04-19 09:51:48 +0300

    feat: ✨ add spartakiada manager

---
+++ src/main/kotlin/ru/joutak/sg/games/SpartakiadaManager.kt
@@ -0,0 +1,8 @@
+package ru.joutak.sg.games
+
+import ru.joutak.sg.utils.PluginManager
+import java.io.File
+
+object SpartakiadaManager {
+    val spartakiadaFolder = File(PluginManager.survivalGames.dataFolder, "spartakiada")
+}
--- src/main/kotlin/ru/joutak/sg/utils/PluginManager.kt
+++ src/main/kotlin/ru/joutak/sg/utils/PluginManager.kt
@@ -3,6 +3,10 @@ package ru.joutak.sg.utils
 import com.onarandombox.MultiverseCore.MultiverseCore
 import org.bukkit.Bukkit
 import ru.joutak.sg.SurvivalGamesPlugin
+import ru.joutak.sg.config.Config
+import ru.joutak.sg.config.ConfigKeys
+import ru.joutak.sg.games.SpartakiadaManager
+import java.io.File
 import java.util.logging.Logger
 
 object PluginManager {
@@ -10,4 +14,11 @@ object PluginManager {
     val multiverseCore: MultiverseCore = Bukkit.getServer().pluginManager.getPlugin("Multiverse-Core") as MultiverseCore
 
     fun getLogger(): Logger = survivalGames.logger
+
+    fun getDataFolder(): File {
+        if (Config.get(ConfigKeys.SPARTAKIADA_MODE)) {
+            return SpartakiadaManager.spartakiadaFolder
+        }
+        return survivalGames.dataFolder
+    }
 }
